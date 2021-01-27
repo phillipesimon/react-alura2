79e43be0dea589a2a28c080ffa44f5f77ea7c3a1
phillipesimon <phillipe.simon@gmail.com>
2021-01-27 03:58:04 -0300

    Aula 02 Finalizada

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
@@ -20,29 +22,43 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
 
     <QuizBackground backgroundImage={db.bg}>
       <Head>
-        <title>What this is?</title>
+        <title>Quiz - Counter-Strike</title>
       </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
           <Widget.Header>
-            <h1>{db.title}</h1>
+            <h1>Counter-Strike: Global Offensive</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Nickname"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
         <Widget>
-          <Widget.Content>
-            <h1>Quizes da Galera</h1>
-
-            <p>lorem ipsum dolor sit amet...</p>
-          </Widget.Content>
+          <Widget.Content />
         </Widget>
         <Footer />
       </QuizContainer>
