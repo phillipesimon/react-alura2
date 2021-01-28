ef2cfa1a861db6aa5284572a38d679809f4e969d
phillipesimon <phillipe.simon@gmail.com>
2021-01-28 04:58:16 -0300

    Final Aula 03

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import Head from 'next/head';
-import styled from 'styled-components';
 import { useRouter } from 'next/router';
 
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import Input from '../src/components/Input';
+import Button from '../src/components/Buttton';
 
 export default function Home() {
   const router = useRouter();
@@ -29,7 +20,7 @@ export default function Home() {
 
     <QuizBackground backgroundImage={db.bg}>
       <Head>
-        <title>Quiz - Counter-Strike</title>
+        <title>Home Quiz - Counter-Strike</title>
       </Head>
       <QuizContainer>
         <QuizLogo />
@@ -38,27 +29,29 @@ export default function Home() {
             <h1>Counter-Strike: Global Offensive</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={function (infosDoEvento) {
+            <form onSubmit={(infosDoEvento) => {
               infosDoEvento.preventDefault();
               router.push(`/quiz?name=${name}`);
             }}
             >
-              <input
-                onChange={function (infosDoEvento) {
-                  setName(infosDoEvento.target.value);
-                }}
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
                 placeholder="Nickname"
+                value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar
-                {name}
-              </button>
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
 
         <Widget>
-          <Widget.Content />
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+            <p>Projeto em desenvolvimento na Segunda Imersão React Alura.</p>
+          </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
